3a27ae2642987fc1e7b79e8dc8c703109779262a
anniez32 <anniez32@nycstudents.net>
2024-10-08 16:47:37 -0400

    Zhao_Annie_Lecture_5
    annie zhao lecture 5

--- Zhao_Annie_Lecture_5.cpp
+++ Zhao_Annie_Lecture_5.cpp
@@ -4,39 +4,45 @@
 
 #include <iostream>
 #include <iomanip>
-#include <string>
 using namespace std;
 
 int main () {
 
     // Variables
-    string transaction; // Type of transaction
+    char transaction; // Type of transaction
     double total;  // Money in the bank account
-    double transfAmt;  // Transfer amount
+    double transfAmtD = 0;  // Transfer amount (debit)
+    double transfAmtC = 0; // Transfer amount (credit)
+    double totalD; // Total Debit amount
+    double totalC; // Total Credit amount
     char again; // Loop for another transaction
+
+    // Get original amount of money in account
+    cout << "How much money did you have at the start of the day? : ";
+    cin >> total;
 
     // Start loop
     cout << "Would you like to make a transaction? y/n : ";
     cin >> again;
     
-    if (again == 'y' || again == 'Y') {
-
-    // Ask for input
-    cout << "How much money did you have at the start of the day? : ";
-    cin >> total;
-
     // Loop 
     while (again == 'y' || again == 'Y') {
-    cout << "What is the type of transaction? debit or credit? : ";
-    cin >> transaction;
-    cout << "How much is the transaction? : ";
-    cin >> transfAmt;
+        cout << "What is the type of transaction? debit or credit? d/c : ";
+        cin >> transaction;
 
-    // IF it is debit or credit
-    if (transaction == "debit" || transaction == "Debit")
-        total = (total - transfAmt);
-    else if (transaction == "credit" || transaction == "Credit")
-        total = (total + transfAmt);
+        // If it is debit or credit
+        if (transaction == 'd' || transaction == 'D') {
+            cout << "How much is the transaction? : ";
+            cin >> transfAmtD; 
+            total = (total - transfAmtD);
+            totalD = (totalD + transfAmtD);
+        }
+        else if (transaction == 'c' || 'C') {
+            cout << "How much is the transaction? : ";
+            cin >> transfAmtC;
+            total = (total + transfAmtC);
+            totalC = (totalC + transfAmtC);
+        }
 
     // Print the total after the transaction
     cout << fixed << setprecision(2);
@@ -45,28 +51,30 @@ int main () {
     //Ask if user would like to input another transaction
     cout << "Do you have any other transactions to make? y/n : ";
     cin >> again;
-    
     }
-    cout << "Have a great day!";
-    }
+    // Ending Message
+    cout << "Have a great day!" << endl;
+    cout << "The total in your account at the end of the day is : $" << total << endl;
+    cout << "The total debit amount is: $" << transfAmtD << endl;
+    cout << "The total credit amount is: $" << transfAmtC << endl;
 
-    else
-        cout << "Have a great day!";
-
-    
     return 0;
 }
+
 // Output Message
 /*
+How much money did you have at the start of the day? : 700
 Would you like to make a transaction? y/n : y
-How much money did you have at the start of the day? : 300
-What is the type of transaction? debit or credit? : debit
-How much is the transaction? : 200
-The total in your account now is : $100.00
+What is the type of transaction? debit or credit? d/c : d
+How much is the transaction? : 777
+The total in your account now is : $-77.00
 Do you have any other transactions to make? y/n : y
-What is the type of transaction? debit or credit? : credit
-How much is the transaction? : 7000
-The total in your account now is : $7100.00
+What is the type of transaction? debit or credit? d/c : c
+How much is the transaction? : 345
+The total in your account now is : $268.00
 Do you have any other transactions to make? y/n : n
 Have a great day!
+The total in your account at the end of the day is : $268.00
+The total debit amount is: $777.00
+The total credit amount is: $345.00
 */
